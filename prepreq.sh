361c9e6b6b71e6d9981c25ac0d4fc7f5b49d43b7
ariveamar <41986809+ariveamar@users.noreply.github.com>
2023-11-30 10:48:16 +0700

    Create prepreq.sh

---
+++ prepreq.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo subscription-manager release --set=9.2
+sudo subscription-manager repos --disable=*
+sudo subscription-manager repos --enable=rhel-9-for-x86_64-baseos-eus-rpms --enable=rhel-9-for-x86_64-appstream-eus-rpms --enable=rhel-9-for-x86_64-highavailability-eus-rpms --enable=openstack-17.1-for-rhel-9-x86_64-rpms --enable=fast-datapath-for-rhel-9-x86_64-rpms
+sudo dnf update -y
+sudo dnf install -y python3-tripleoclient
+sudo reboot
