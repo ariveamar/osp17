dd6b84f25938480a753b528f627bab16fb18a77e
ariveamar <41986809+ariveamar@users.noreply.github.com>
2023-12-05 20:44:52 +0700

    Update prepreq.sh

--- prepreq.sh
+++ prepreq.sh
@@ -4,4 +4,5 @@ sudo subscription-manager repos --disable=*
 sudo subscription-manager repos --enable=rhel-9-for-x86_64-baseos-eus-rpms --enable=rhel-9-for-x86_64-appstream-eus-rpms --enable=rhel-9-for-x86_64-highavailability-eus-rpms --enable=openstack-17.1-for-rhel-9-x86_64-rpms --enable=fast-datapath-for-rhel-9-x86_64-rpms
 sudo dnf update -y
 sudo dnf install -y python3-tripleoclient
+openstack tripleo container image prepare default  --local-push-destination --output-env-file /home/stack/containers-prepare-parameter.yaml
 sudo reboot
